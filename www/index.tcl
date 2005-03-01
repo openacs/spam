3c8fba61b79d39440917952bed82c1265b8f7ba7
Jeff Davis <davis@xarg.net>
2005-03-01 00:01:44 +0000

    change ad_verify_and_get_user_id to ad_conn user_id, change ad_maybe_redirect_for_registration to auth::require_login, make some db_ and dt_ function -public

--- www/index.tcl
+++ www/index.tcl
@@ -13,7 +13,7 @@ ad_page_contract {
     context:onevalue
 }
 
-set user_id [ad_maybe_redirect_for_registration]
+set user_id [auth::require_login]
 
 # a message is considered unsent if sent_p is false
 
